9b9723da93ce0d616808550a8a442134cf72412f
Alex <74885892+misspoken69@users.noreply.github.com>
2021-03-30 12:31:26 -0700

    Create ready.js

---
+++ events/ready.js
@@ -0,0 +1,22 @@
+module.exports = (client) => {
+    console.log('Login as' + client.user.tag)
+    console.log(`Aero©`);
+    console.log(`Aero©`);
+    console.log(`Aero©`);
+    console.log(`Aero©`);
+    console.log(`Aero©`);
+    console.log(`Aero©`);
+    console.log(`Aero©`);
+    console.log(`Aero©`);
+    console.log(`Aero©`);
+    console.log(`Aero©`);
+    console.log(`Aero©`);
+    console.log(`Aero©`);
+    console.log(`Aero©`);
+    console.log(`Aero©`);
+    console.log(`Aero©`);
+    console.log(`Aero©`);
+    console.log(`Aero©`);
+    console.log(`Aero©`);
+    console.log(`Aero©`);
+}
